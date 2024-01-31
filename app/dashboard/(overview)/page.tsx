ad5261ede794b7f2b9e5182e27d2250e83371412
Mariano Belladonna <upfordev@gmail.com>
2024-01-31 19:38:12 -0300

    Removing ts-expect-error directives to see if build succeeds

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -13,17 +13,14 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          {/*@ts-expect-error Async Server Componenr*/}
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          {/*@ts-expect-error Async Server Componenr*/}
           <RevenueChart  />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          {/*@ts-expect-error Async Server Componenr*/}
           <LatestInvoices />
         </Suspense>
       </div>
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -33,7 +33,6 @@ export default async function Page({
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        {/*@ts-expect-error Async Server Componenr*/}
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
